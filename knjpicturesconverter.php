cde9c753c1ef91ca8d2e12de3d1c970a0be1b01b
Kasper Johansen <k@spernj.org>
2011-03-29 16:58:07 +0200

    Added danish translations.

--- knjpicturesconverter.php
+++ knjpicturesconverter.php
@@ -1,14 +1,14 @@
 #!/usr/bin/php53gtk
 <?php
-	chdir(dirname(__FILE__));
-	
-	require_once("knjphpframework/functions_knj_extensions.php");
-	knj_dl(array("gd", "gtk2"));
-	
-	require_once("knjphpframework/functions_knj_locales.php");
-	knjlocales_setmodule("locales", "locales");
-	
-	require_once("windows/win_main.php");
-	$win_main = new WinMain();
-	Gtk::main();
-?>
+
+chdir(dirname(__FILE__));
+
+require_once("knjphpframework/functions_knj_extensions.php");
+knj_dl(array("gd", "gtk2"));
+
+require_once("knjphpframework/functions_knj_locales.php");
+knjlocales_setmodule("locales", "locales");
+
+require_once("windows/win_main.php");
+$win_main = new WinMain();
+Gtk::main();
